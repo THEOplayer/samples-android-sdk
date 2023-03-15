8eb768b14735748ff0d0af79266d61293fca2ca5
OlegRyz <aleh.ryzhankou@gmail.com>
2023-03-15 17:07:47 +0100

    Convert code to Kotlin using android studio converter

--- Basic-Playback/app/src/main/java/com/theoplayer/sample/playback/basic/PlayerActivity.kt
+++ Basic-Playback/app/src/main/java/com/theoplayer/sample/playback/basic/PlayerActivity.kt
@@ -1,92 +1,108 @@
-package com.theoplayer.sample.playback.basic;
+package com.theoplayer.sample.playback.basic
 
-import android.os.Bundle;
-import android.util.Log;
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.theoplayer.android.api.event.player.*
+import com.theoplayer.android.api.player.Player
+import com.theoplayer.android.api.source.SourceDescription
+import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.sample.playback.basic.databinding.ActivityPlayerBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.theoplayer.android.api.event.player.PlayerEventTypes;
-import com.theoplayer.android.api.player.Player;
-import com.theoplayer.android.api.source.SourceDescription;
-import com.theoplayer.android.api.source.TypedSource;
-import com.theoplayer.sample.playback.basic.databinding.ActivityPlayerBinding;
-
-public class PlayerActivity extends AppCompatActivity {
-
-    private static final String TAG = PlayerActivity.class.getSimpleName();
-
-    private ActivityPlayerBinding viewBinding;
-    private Player theoPlayer;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.TheoTheme_Base);
-        super.onCreate(savedInstanceState);
+class PlayerActivity : AppCompatActivity() {
+    private var viewBinding: ActivityPlayerBinding? = null
+    private var theoPlayer: Player? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.TheoTheme_Base)
+        super.onCreate(savedInstanceState)
 
         // Inflating view and obtaining an instance of the binding class.
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player);
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player)
 
         // Gathering THEO objects references.
-        theoPlayer = viewBinding.theoPlayerView.getPlayer();
+        theoPlayer = viewBinding.theoPlayerView.player
 
         // Configuring action bar.
-        setSupportActionBar(viewBinding.toolbarLayout.toolbar);
+        setSupportActionBar(viewBinding.toolbarLayout.toolbar)
 
         // Configuring THEOplayer playback with default parameters.
-        configureTHEOplayer();
+        configureTHEOplayer()
     }
 
-    private void configureTHEOplayer() {
+    private fun configureTHEOplayer() {
         // Coupling the orientation of the device with the fullscreen state.
         // The player will go fullscreen when the device is rotated to landscape
         // and will also exit fullscreen when the device is rotated back to portrait.
-        viewBinding.theoPlayerView.getSettings().setFullScreenOrientationCoupled(true);
+        viewBinding!!.theoPlayerView.settings.isFullScreenOrientationCoupled = true
 
         // Creating a TypedSource builder that defines the location of a single stream source.
-        TypedSource.Builder typedSource = new TypedSource.Builder(getString(R.string.defaultSourceUrl));
+        val typedSource = TypedSource.Builder(getString(R.string.defaultSourceUrl))
 
         // Creating a SourceDescription builder that contains the settings to be applied as a new
         // THEOplayer source.
-        SourceDescription.Builder sourceDescription = new SourceDescription.Builder(typedSource.build())
-                .poster(getString(R.string.defaultPosterUrl));
+        val sourceDescription = SourceDescription.Builder(typedSource.build())
+            .poster(getString(R.string.defaultPosterUrl))
 
         // Configuring THEOplayer with defined SourceDescription object.
-        theoPlayer.setSource(sourceDescription.build());
+        theoPlayer!!.source = sourceDescription.build()
 
         // Configuring video to play whenever THEOplayer is visible to a User.
-        theoPlayer.setAutoplay(true);
+        theoPlayer!!.isAutoplay = true
 
         // Adding listeners to THEOplayer basic playback events.
-        theoPlayer.addEventListener(PlayerEventTypes.PLAY, event -> Log.i(TAG, "Event: PLAY"));
-        theoPlayer.addEventListener(PlayerEventTypes.PLAYING, event -> Log.i(TAG, "Event: PLAYING"));
-        theoPlayer.addEventListener(PlayerEventTypes.PAUSE, event -> Log.i(TAG, "Event: PAUSE"));
-        theoPlayer.addEventListener(PlayerEventTypes.ENDED, event -> Log.i(TAG, "Event: ENDED"));
-        theoPlayer.addEventListener(PlayerEventTypes.ERROR, event -> Log.i(TAG, "Event: ERROR, error=" + event.getErrorObject()));
+        theoPlayer!!.addEventListener(PlayerEventTypes.PLAY) { event: PlayEvent? ->
+            Log.i(
+                TAG,
+                "Event: PLAY"
+            )
+        }
+        theoPlayer!!.addEventListener(PlayerEventTypes.PLAYING) { event: PlayingEvent? ->
+            Log.i(
+                TAG,
+                "Event: PLAYING"
+            )
+        }
+        theoPlayer!!.addEventListener(PlayerEventTypes.PAUSE) { event: PauseEvent? ->
+            Log.i(
+                TAG,
+                "Event: PAUSE"
+            )
+        }
+        theoPlayer!!.addEventListener(PlayerEventTypes.ENDED) { event: EndedEvent? ->
+            Log.i(
+                TAG,
+                "Event: ENDED"
+            )
+        }
+        theoPlayer!!.addEventListener(PlayerEventTypes.ERROR) { event: ErrorEvent ->
+            Log.i(
+                TAG,
+                "Event: ERROR, error=" + event.errorObject
+            )
+        }
     }
-
 
     // In order to work properly and in sync with the activity lifecycle changes (e.g. device
     // is rotated, new activity is started or app is moved to background) we need to call
     // the "onResume", "onPause" and "onDestroy" methods of the THEOplayerView when the matching
     // activity methods are called.
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        viewBinding.theoPlayerView.onPause();
+    override fun onPause() {
+        super.onPause()
+        viewBinding!!.theoPlayerView.onPause()
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        viewBinding.theoPlayerView.onResume();
+    override fun onResume() {
+        super.onResume()
+        viewBinding!!.theoPlayerView.onResume()
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        viewBinding.theoPlayerView.onDestroy();
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding!!.theoPlayerView.onDestroy()
     }
 
+    companion object {
+        private val TAG = PlayerActivity::class.java.simpleName
+    }
 }
