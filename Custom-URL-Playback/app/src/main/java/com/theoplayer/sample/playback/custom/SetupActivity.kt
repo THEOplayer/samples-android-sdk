ee0cfd12bd8b12b2cd87ec91f3fa2d10393179d3
OlegRyz <aleh.ryzhankou@gmail.com>
2023-03-29 09:35:01 +0200

    Convert into Kotlin

--- Custom-URL-Playback/app/src/main/java/com/theoplayer/sample/playback/custom/SetupActivity.kt
+++ Custom-URL-Playback/app/src/main/java/com/theoplayer/sample/playback/custom/SetupActivity.kt
@@ -1,111 +1,116 @@
-package com.theoplayer.sample.playback.custom;
+package com.theoplayer.sample.playback.custom
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.view.inputmethod.InputMethodManager
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.theoplayer.sample.playback.custom.PlayerActivity.Companion.play
+import com.theoplayer.sample.playback.custom.databinding.ActivitySetupBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.theoplayer.sample.playback.custom.databinding.ActivitySetupBinding;
-
-
-public class SetupActivity extends AppCompatActivity {
-
-    private ActivitySetupBinding viewBinding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.TheoTheme_Base);
-        super.onCreate(savedInstanceState);
+class SetupActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivitySetupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.TheoTheme_Base)
+        super.onCreate(savedInstanceState)
 
         // Inflating view and obtaining an instance of the binding class.
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_setup);
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_setup)
 
         // Configuring action bar.
-        setSupportActionBar(viewBinding.toolbarLayout.toolbar);
+        setSupportActionBar(viewBinding.toolbarLayout.toolbar)
 
         // Configure UI behavior and default values.
-        configureUI();
+        configureUI()
     }
 
-    private void configureUI() {
+    private fun configureUI() {
         // Defining default sources and its visibility constraints.
-        viewBinding.sourceUrlTextInput.setTag(R.id.streamTypeClear, getString(R.string.defaultClearSourceUrl));
-        viewBinding.sourceUrlTextInput.setTag(R.id.streamTypeProtected, getString(R.string.defaultProtectedSourceUrl));
-        viewBinding.sourceUrlTextInput.setOnFocusChangeListener((view, hasFocus) -> onTextInputFocusChange((TextView) view));
+        viewBinding.sourceUrlTextInput.setTag(
+            R.id.streamTypeClear,
+            getString(R.string.defaultClearSourceUrl)
+        )
+        viewBinding.sourceUrlTextInput.setTag(
+            R.id.streamTypeProtected,
+            getString(R.string.defaultProtectedSourceUrl)
+        )
+        viewBinding.sourceUrlTextInput.onFocusChangeListener =
+            OnFocusChangeListener { view: View, hasFocus: Boolean -> onTextInputFocusChange(view as TextView) }
 
         // Defining default licenses and its visibility constraints.
-        viewBinding.licenseUrlTextInput.setTag(R.id.streamTypeProtected, getString(R.string.defaultProtectedLicenseUrl));
-        viewBinding.licenseUrlTextInput.setOnFocusChangeListener((view, hasFocus) -> onTextInputFocusChange((TextView) view));
+        viewBinding.licenseUrlTextInput.setTag(
+            R.id.streamTypeProtected,
+            getString(R.string.defaultProtectedLicenseUrl)
+        )
+        viewBinding.licenseUrlTextInput.onFocusChangeListener =
+            OnFocusChangeListener { view: View, hasFocus: Boolean -> onTextInputFocusChange(view as TextView) }
 
         // Checking default stream type and defining action on its change.
-        viewBinding.streamTypesGroup.setOnCheckedChangeListener((group, checkedId) -> onStreamTypeChange(checkedId));
-        viewBinding.streamTypesGroup.check(R.id.streamTypeClear);
+        viewBinding.streamTypesGroup.setOnCheckedChangeListener { group: RadioGroup?, checkedId: Int ->
+            onStreamTypeChange(
+                checkedId
+            )
+        }
+        viewBinding.streamTypesGroup.check(R.id.streamTypeClear)
 
         // Setting action on stream play request.
-        viewBinding.playButton.setOnClickListener(playButton -> onPlay());
+        viewBinding.playButton.setOnClickListener { playButton: View? -> onPlay() }
     }
 
-    private void onTextInputFocusChange(TextView textInput) {
-        changeDefaultValueVisibilityForTextInput(textInput);
+    private fun onTextInputFocusChange(textInput: TextView) {
+        changeDefaultValueVisibilityForTextInput(textInput)
 
         // Focus can be lost by clicking outside text input, in this case soft keyboard
         // needs to be hidden manually if not needed anymore.
-        if (!viewBinding.sourceUrlTextInput.isFocused() && !viewBinding.licenseUrlTextInput.isFocused()) {
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-            if (inputMethodManager != null) {
-                inputMethodManager.hideSoftInputFromWindow(textInput.getWindowToken(), 0);
-            }
+        if (!viewBinding.sourceUrlTextInput.isFocused && !viewBinding.licenseUrlTextInput.isFocused) {
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(textInput.windowToken, 0)
         }
     }
 
-    private void onStreamTypeChange(int checkedStreamTypeId) {
-        changeDefaultValueForTextInput(viewBinding.sourceUrlTextInput, checkedStreamTypeId);
-        changeDefaultValueVisibilityForTextInput(viewBinding.sourceUrlTextInput);
-
-        changeDefaultValueForTextInput(viewBinding.licenseUrlTextInput, checkedStreamTypeId);
-        changeDefaultValueVisibilityForTextInput(viewBinding.licenseUrlTextInput);
-
-        boolean isProtectedStreamChecked = R.id.streamTypeProtected == checkedStreamTypeId;
-        viewBinding.licenseUrlTextInput.setEnabled(isProtectedStreamChecked);
+    private fun onStreamTypeChange(checkedStreamTypeId: Int) {
+        changeDefaultValueForTextInput(viewBinding.sourceUrlTextInput, checkedStreamTypeId)
+        changeDefaultValueVisibilityForTextInput(viewBinding.sourceUrlTextInput)
+        changeDefaultValueForTextInput(viewBinding.licenseUrlTextInput, checkedStreamTypeId)
+        changeDefaultValueVisibilityForTextInput(viewBinding.licenseUrlTextInput)
+        val isProtectedStreamChecked = R.id.streamTypeProtected == checkedStreamTypeId
+        viewBinding.licenseUrlTextInput.isEnabled = isProtectedStreamChecked
     }
 
-    private void changeDefaultValueForTextInput(TextView textInput, int defaultValueId) {
-        CharSequence currentDefaultValue = (CharSequence) textInput.getTag();
-        CharSequence newDefaultValue = (CharSequence) textInput.getTag(defaultValueId);
+    private fun changeDefaultValueForTextInput(textInput: TextView, defaultValueId: Int) {
+        val currentDefaultValue = textInput.tag as CharSequence
+        val newDefaultValue = textInput.getTag(defaultValueId) as CharSequence
 
         // Clearing currently displayed value if equal to default value.
-        if (TextUtils.equals(textInput.getText(), currentDefaultValue)) {
-            textInput.setText("");
+        if (TextUtils.equals(textInput.text, currentDefaultValue)) {
+            textInput.text = ""
         }
 
         // Changing default value according to given default value resource id.
-        textInput.setTag(newDefaultValue);
+        textInput.tag = newDefaultValue
     }
 
-    private void changeDefaultValueVisibilityForTextInput(TextView textInput) {
-        CharSequence defaultValue = (CharSequence) textInput.getTag();
+    private fun changeDefaultValueVisibilityForTextInput(textInput: TextView) {
+        val defaultValue = textInput.tag as CharSequence
         // Default value is displayed if text field is empty and not focused.
-        if (TextUtils.equals(textInput.getText(), textInput.isFocused() ? defaultValue : "")) {
-            textInput.setText(textInput.isFocused() ? "" : defaultValue);
+        if (TextUtils.equals(textInput.text, if (textInput.isFocused) defaultValue else "")) {
+            textInput.text = if (textInput.isFocused) "" else defaultValue
         }
     }
 
-    private void onPlay() {
-        CharSequence sourceUrl = viewBinding.sourceUrlTextInput.getText();
-        String defaultSourceUrl = (String) viewBinding.sourceUrlTextInput.getTag();
-
-        CharSequence licenseUrl = viewBinding.licenseUrlTextInput.getText();
-        String defaultLicenseUrl = (String) viewBinding.licenseUrlTextInput.getTag();
-
-        PlayerActivity.play(
-                this,
-                TextUtils.isEmpty(sourceUrl) ? defaultSourceUrl : sourceUrl.toString(),
-                TextUtils.isEmpty(licenseUrl) ? defaultLicenseUrl : licenseUrl.toString()
-        );
+    private fun onPlay() {
+        val sourceUrl: CharSequence? = viewBinding.sourceUrlTextInput.text
+        val defaultSourceUrl = viewBinding.sourceUrlTextInput.tag as String
+        val licenseUrl: CharSequence? = viewBinding.licenseUrlTextInput.text
+        val defaultLicenseUrl = viewBinding.licenseUrlTextInput.tag as String
+        play(
+            this,
+            if (TextUtils.isEmpty(sourceUrl)) defaultSourceUrl else sourceUrl.toString(),
+            if (TextUtils.isEmpty(licenseUrl)) defaultLicenseUrl else licenseUrl.toString()
+        )
     }
-
 }
