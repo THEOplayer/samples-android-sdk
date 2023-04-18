ef561446d0d6850904e654f3e07b2fb3c3ebd676
OlegRyz <aleh.ryzhankou@gmail.com>
2023-04-18 09:32:29 +0200

    Convert CUstom-Ads to Kotlin

--- Custom-Ads/app/src/main/java/com/theoplayer/sample/ads/custom/PlayerActivity.kt
+++ Custom-Ads/app/src/main/java/com/theoplayer/sample/ads/custom/PlayerActivity.kt
@@ -1,122 +1,152 @@
-package com.theoplayer.sample.ads.custom;
+package com.theoplayer.sample.ads.custom
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.Log;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.theoplayer.android.api.event.ads.AdBeginEvent
+import com.theoplayer.android.api.event.ads.AdEndEvent
+import com.theoplayer.android.api.event.ads.AdErrorEvent
+import com.theoplayer.android.api.event.ads.AdsEventTypes
+import com.theoplayer.android.api.event.player.*
+import com.theoplayer.android.api.player.Player
+import com.theoplayer.android.api.source.SourceDescription
+import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
+import com.theoplayer.sample.ads.custom.databinding.ActivityPlayerBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.theoplayer.android.api.event.ads.AdsEventTypes;
-import com.theoplayer.android.api.event.player.PlayerEventTypes;
-import com.theoplayer.android.api.player.Player;
-import com.theoplayer.android.api.source.SourceDescription;
-import com.theoplayer.android.api.source.TypedSource;
-import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription;
-import com.theoplayer.sample.ads.custom.databinding.ActivityPlayerBinding;
-
-public class PlayerActivity extends AppCompatActivity {
-
-    private static final String TAG = PlayerActivity.class.getSimpleName();
-
-    private static final String PLAYER_PARAM__SOURCE_URL = "SOURCE_URL";
-    private static final String PLAYER_PARAM__AD_URL = "AD_URL";
-    private static final String PLAYER_PARAM__AD_TIME_OFFSET = "AD_TIME_OFFSET";
-
-    private ActivityPlayerBinding viewBinding;
-    private Player theoPlayer;
-
-    public static void play(Context context, String sourceUrl, String adUrl, String adTimeOffset) {
-        Intent playIntent = new Intent(context, PlayerActivity.class);
-        playIntent.putExtra(PLAYER_PARAM__SOURCE_URL, sourceUrl);
-        playIntent.putExtra(PLAYER_PARAM__AD_URL, adUrl);
-        playIntent.putExtra(PLAYER_PARAM__AD_TIME_OFFSET, adTimeOffset);
-        context.startActivity(playIntent);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+class PlayerActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityPlayerBinding
+    private lateinit var theoPlayer: Player
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         // Inflating view and obtaining an instance of the binding class.
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player);
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player)
 
         // Gathering THEO objects references.
-        theoPlayer = viewBinding.theoPlayerView.getPlayer();
+        theoPlayer = viewBinding.theoPlayerView.player
 
         // Configuring action bar.
-        setSupportActionBar(viewBinding.toolbarLayout.toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar(viewBinding.toolbarLayout.toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Configuring THEOplayer playback with parameters from intent.
         configureTHEOplayer(
-                getIntent().getStringExtra(PLAYER_PARAM__SOURCE_URL),
-                getIntent().getStringExtra(PLAYER_PARAM__AD_URL),
-                getIntent().getStringExtra(PLAYER_PARAM__AD_TIME_OFFSET)
-        );
+            intent.getStringExtra(PLAYER_PARAM__SOURCE_URL)!!,
+            intent.getStringExtra(PLAYER_PARAM__AD_URL)!!,
+            intent.getStringExtra(PLAYER_PARAM__AD_TIME_OFFSET)!!
+        )
     }
 
-    private void configureTHEOplayer(String sourceUrl, String adUrl, String adTimeOffset) {
+    private fun configureTHEOplayer(sourceUrl: String, adUrl: String, adTimeOffset: String) {
         // Coupling the orientation of the device with the fullscreen state.
         // The player will go fullscreen when the device is rotated to landscape
         // and will also exit fullscreen when the device is rotated back to portrait.
-        viewBinding.theoPlayerView.getSettings().setFullScreenOrientationCoupled(true);
+        viewBinding.theoPlayerView.settings.isFullScreenOrientationCoupled = true
 
         // Creating a TypedSource builder that defines the location of a single stream source.
-        TypedSource.Builder typedSource = new TypedSource.Builder(sourceUrl);
+        val typedSource = TypedSource.Builder(sourceUrl)
 
         // Creating a GoogleImaAdDescription builder that defines the location of an ad and its
         // time offset.
-        GoogleImaAdDescription.Builder adDescription = new GoogleImaAdDescription.Builder(adUrl)
-                .timeOffset(adTimeOffset);
+        val adDescription = GoogleImaAdDescription.Builder(adUrl)
+            .timeOffset(adTimeOffset)
 
         // Creating a SourceDescription builder that contains the settings to be applied as a new
         // THEOplayer source including ads configuration.
-        SourceDescription.Builder sourceDescription = new SourceDescription.Builder(typedSource.build())
-                .ads(adDescription.build());
+        val sourceDescription = SourceDescription.Builder(typedSource.build())
+            .ads(adDescription.build())
 
         // Configuring THEOplayer with defined SourceDescription object to be played automatically.
-        theoPlayer.setSource(sourceDescription.build());
-        theoPlayer.setAutoplay(true);
+        theoPlayer.source = sourceDescription.build()
+        theoPlayer.isAutoplay = true
 
         // Adding listeners to THEOplayer basic playback events.
-        theoPlayer.addEventListener(PlayerEventTypes.PLAY, event -> Log.i(TAG, "Event: PLAY"));
-        theoPlayer.addEventListener(PlayerEventTypes.PLAYING, event -> Log.i(TAG, "Event: PLAYING"));
-        theoPlayer.addEventListener(PlayerEventTypes.PAUSE, event -> Log.i(TAG, "Event: PAUSE"));
-        theoPlayer.addEventListener(PlayerEventTypes.ENDED, event -> Log.i(TAG, "Event: ENDED"));
-        theoPlayer.addEventListener(PlayerEventTypes.ERROR, event -> Log.i(TAG, "Event: ERROR, error=" + event.getErrorObject()));
+        theoPlayer.addEventListener(PlayerEventTypes.PLAY) { event: PlayEvent? ->
+            Log.i(
+                TAG,
+                "Event: PLAY"
+            )
+        }
+        theoPlayer.addEventListener(PlayerEventTypes.PLAYING) { event: PlayingEvent? ->
+            Log.i(
+                TAG,
+                "Event: PLAYING"
+            )
+        }
+        theoPlayer.addEventListener(PlayerEventTypes.PAUSE) { event: PauseEvent? ->
+            Log.i(
+                TAG,
+                "Event: PAUSE"
+            )
+        }
+        theoPlayer.addEventListener(PlayerEventTypes.ENDED) { event: EndedEvent? ->
+            Log.i(
+                TAG,
+                "Event: ENDED"
+            )
+        }
+        theoPlayer.addEventListener(PlayerEventTypes.ERROR) { event: ErrorEvent ->
+            Log.i(
+                TAG,
+                "Event: ERROR, error=" + event.errorObject
+            )
+        }
 
         // Adding listeners to THEOplayer basic ad events.
-        theoPlayer.getAds().addEventListener(AdsEventTypes.AD_BEGIN, event -> Log.i(TAG, "Event: AD_BEGIN, ad=" + event.getAd()));
-        theoPlayer.getAds().addEventListener(AdsEventTypes.AD_END, event -> Log.i(TAG, "Event: AD_END, ad=" + event.getAd()));
-        theoPlayer.getAds().addEventListener(AdsEventTypes.AD_ERROR, event -> Log.i(TAG, "Event: AD_ERROR, error=" + event.getError()));
+        theoPlayer.ads.addEventListener(AdsEventTypes.AD_BEGIN) { event: AdBeginEvent ->
+            Log.i(
+                TAG, "Event: AD_BEGIN, ad=" + event.ad
+            )
+        }
+        theoPlayer.ads.addEventListener(AdsEventTypes.AD_END) { event: AdEndEvent ->
+            Log.i(
+                TAG,
+                "Event: AD_END, ad=" + event.ad
+            )
+        }
+        theoPlayer.ads.addEventListener(AdsEventTypes.AD_ERROR) { event: AdErrorEvent ->
+            Log.i(
+                TAG, "Event: AD_ERROR, error=" + event.error
+            )
+        }
     }
-
 
     // In order to work properly and in sync with the activity lifecycle changes (e.g. device
     // is rotated, new activity is started or app is moved to background) we need to call
     // the "onResume", "onPause" and "onDestroy" methods of the THEOplayerView when the matching
     // activity methods are called.
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        viewBinding.theoPlayerView.onPause();
+    override fun onPause() {
+        super.onPause()
+        viewBinding.theoPlayerView.onPause()
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        viewBinding.theoPlayerView.onResume();
+    override fun onResume() {
+        super.onResume()
+        viewBinding.theoPlayerView.onResume()
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        viewBinding.theoPlayerView.onDestroy();
+    override fun onDestroy() {
+        super.onDestroy()
+        viewBinding.theoPlayerView.onDestroy()
     }
 
+    companion object {
+        private val TAG = PlayerActivity::class.java.simpleName
+        private const val PLAYER_PARAM__SOURCE_URL = "SOURCE_URL"
+        private const val PLAYER_PARAM__AD_URL = "AD_URL"
+        private const val PLAYER_PARAM__AD_TIME_OFFSET = "AD_TIME_OFFSET"
+        @JvmStatic
+        fun play(context: Context, sourceUrl: String?, adUrl: String?, adTimeOffset: String?) {
+            val playIntent = Intent(context, PlayerActivity::class.java)
+            playIntent.putExtra(PLAYER_PARAM__SOURCE_URL, sourceUrl)
+            playIntent.putExtra(PLAYER_PARAM__AD_URL, adUrl)
+            playIntent.putExtra(PLAYER_PARAM__AD_TIME_OFFSET, adTimeOffset)
+            context.startActivity(playIntent)
+        }
+    }
 }
--- Custom-Ads/app/src/main/java/com/theoplayer/sample/ads/custom/SetupActivity.kt
+++ Custom-Ads/app/src/main/java/com/theoplayer/sample/ads/custom/SetupActivity.kt
@@ -1,124 +1,135 @@
-package com.theoplayer.sample.ads.custom;
+package com.theoplayer.sample.ads.custom
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.view.View.OnFocusChangeListener
+import android.view.inputmethod.InputMethodManager
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.theoplayer.sample.ads.custom.PlayerActivity.Companion.play
+import com.theoplayer.sample.ads.custom.databinding.ActivitySetupBinding
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
-import com.theoplayer.sample.ads.custom.databinding.ActivitySetupBinding;
-
-
-public class SetupActivity extends AppCompatActivity {
-
-    private ActivitySetupBinding viewBinding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.TheoTheme_Base);
-        super.onCreate(savedInstanceState);
+class SetupActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivitySetupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.TheoTheme_Base)
+        super.onCreate(savedInstanceState)
 
         // Inflating view and obtaining an instance of the binding class.
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_setup);
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_setup)
 
         // Configuring action bar.
-        setSupportActionBar(viewBinding.toolbarLayout.toolbar);
+        setSupportActionBar(viewBinding.toolbarLayout.toolbar)
 
         // Configure UI behavior and default values.
-        configureUI();
+        configureUI()
     }
 
-    private void configureUI() {
+    private fun configureUI() {
         // Defining default ads and its visibility constraints.
-        viewBinding.adUrlTextInput.setTag(R.id.adStandardVast, getString(R.string.defaultVastAdUrl));
-        viewBinding.adUrlTextInput.setTag(R.id.adStandardVmap, getString(R.string.defaultVmapAdUrl));
-        viewBinding.adUrlTextInput.setOnFocusChangeListener((view, hasFocus) -> onTextInputFocusChange((TextView) view));
+        viewBinding.adUrlTextInput.setTag(
+            R.id.adStandardVast,
+            getString(R.string.defaultVastAdUrl)
+        )
+        viewBinding.adUrlTextInput.setTag(
+            R.id.adStandardVmap,
+            getString(R.string.defaultVmapAdUrl)
+        )
+        viewBinding.adUrlTextInput.onFocusChangeListener =
+            OnFocusChangeListener { view: View, hasFocus: Boolean -> onTextInputFocusChange(view as TextView) }
 
         // Defining default sources and its visibility constraints.
-        viewBinding.sourceUrlTextInput.setTag(getString(R.string.defaultSourceUrl));
-        viewBinding.sourceUrlTextInput.setOnFocusChangeListener((view, hasFocus) -> onTextInputFocusChange((TextView) view));
+        viewBinding.sourceUrlTextInput.tag = getString(R.string.defaultSourceUrl)
+        viewBinding.sourceUrlTextInput.onFocusChangeListener =
+            OnFocusChangeListener { view: View, hasFocus: Boolean -> onTextInputFocusChange(view as TextView) }
 
         // Defining ad placement values and checking default ad placement.
-        viewBinding.adPlacementsGroup.setTag(R.id.adPlacementPreRoll, getString(R.string.defaultPreRollAddPlacement));
-        viewBinding.adPlacementsGroup.setTag(R.id.adPlacementMidRoll, getString(R.string.defaultMidRollAddPlacement));
-        viewBinding.adPlacementsGroup.setTag(R.id.adPlacementPostRoll, getString(R.string.defaultPostRollAddPlacement));
-        viewBinding.adPlacementsGroup.check(R.id.adPlacementPreRoll);
+        viewBinding.adPlacementsGroup.setTag(
+            R.id.adPlacementPreRoll,
+            getString(R.string.defaultPreRollAddPlacement)
+        )
+        viewBinding.adPlacementsGroup.setTag(
+            R.id.adPlacementMidRoll,
+            getString(R.string.defaultMidRollAddPlacement)
+        )
+        viewBinding.adPlacementsGroup.setTag(
+            R.id.adPlacementPostRoll,
+            getString(R.string.defaultPostRollAddPlacement)
+        )
+        viewBinding.adPlacementsGroup.check(R.id.adPlacementPreRoll)
 
         // Checking default ad standard and defining action on its change.
-        viewBinding.adStandardsGroup.setOnCheckedChangeListener((group, checkedId) -> onAdStandardChange(checkedId));
-        viewBinding.adStandardsGroup.check(R.id.adStandardVast);
+        viewBinding.adStandardsGroup.setOnCheckedChangeListener { group: RadioGroup?, checkedId: Int ->
+            onAdStandardChange(
+                checkedId
+            )
+        }
+        viewBinding.adStandardsGroup.check(R.id.adStandardVast)
 
         // Setting action on stream play request.
-        viewBinding.playButton.setOnClickListener(playButton -> onPlay());
+        viewBinding.playButton.setOnClickListener { playButton: View? -> onPlay() }
     }
 
-    private void onTextInputFocusChange(TextView textInput) {
-        changeDefaultValueVisibilityForTextInput(textInput);
+    private fun onTextInputFocusChange(textInput: TextView) {
+        changeDefaultValueVisibilityForTextInput(textInput)
 
         // Focus can be lost by clicking outside text input, in this case soft keyboard
         // needs to be hidden manually if not needed anymore.
-        if (!viewBinding.sourceUrlTextInput.isFocused() && !viewBinding.adUrlTextInput.isFocused()) {
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-            if (inputMethodManager != null) {
-                inputMethodManager.hideSoftInputFromWindow(textInput.getWindowToken(), 0);
-            }
+        if (!viewBinding.sourceUrlTextInput.isFocused && !viewBinding.adUrlTextInput.isFocused) {
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(textInput.windowToken, 0)
         }
     }
 
-    private void onAdStandardChange(int checkedAdStandardId) {
-        changeDefaultValueForTextInput(viewBinding.adUrlTextInput, checkedAdStandardId);
-        changeDefaultValueVisibilityForTextInput(viewBinding.adUrlTextInput);
-
-        changeDefaultValueVisibilityForTextInput(viewBinding.sourceUrlTextInput);
+    private fun onAdStandardChange(checkedAdStandardId: Int) {
+        changeDefaultValueForTextInput(viewBinding.adUrlTextInput, checkedAdStandardId)
+        changeDefaultValueVisibilityForTextInput(viewBinding.adUrlTextInput)
+        changeDefaultValueVisibilityForTextInput(viewBinding.sourceUrlTextInput)
 
         // VMAP standard already defines ad placement, redefining it has no sense.
-        boolean isVmapNotChecked = R.id.adStandardVmap != checkedAdStandardId;
-        for (int i = 0; i < viewBinding.adPlacementsGroup.getChildCount(); i++) {
-            viewBinding.adPlacementsGroup.getChildAt(i).setEnabled(isVmapNotChecked);
+        val isVmapNotChecked = R.id.adStandardVmap != checkedAdStandardId
+        for (i in 0 until viewBinding.adPlacementsGroup.childCount) {
+            viewBinding.adPlacementsGroup.getChildAt(i).isEnabled = isVmapNotChecked
         }
     }
 
-    private void changeDefaultValueForTextInput(TextView textInput, int defaultValueId) {
-        CharSequence currentDefaultValue = (CharSequence) textInput.getTag();
-        CharSequence newDefaultValue = (CharSequence) textInput.getTag(defaultValueId);
+    private fun changeDefaultValueForTextInput(textInput: TextView, defaultValueId: Int) {
+        val currentDefaultValue = (textInput.tag ?: "") as CharSequence
+        val newDefaultValue = textInput.getTag(defaultValueId) as CharSequence
 
         // Clearing currently displayed value if equal to default value.
-        if (TextUtils.equals(textInput.getText(), currentDefaultValue)) {
-            textInput.setText("");
+        if (TextUtils.equals(textInput.text, currentDefaultValue)) {
+            textInput.text = ""
         }
 
         // Changing default value according to given default value resource id.
-        textInput.setTag(newDefaultValue);
+        textInput.tag = newDefaultValue
     }
 
-    private void changeDefaultValueVisibilityForTextInput(TextView textInput) {
-        CharSequence defaultValue = (CharSequence) textInput.getTag();
+    private fun changeDefaultValueVisibilityForTextInput(textInput: TextView) {
+        val defaultValue = (textInput.tag ?: "")  as CharSequence
         // Default value is displayed if text field is empty and not focused.
-        if (TextUtils.equals(textInput.getText(), textInput.isFocused() ? defaultValue : "")) {
-            textInput.setText(textInput.isFocused() ? "" : defaultValue);
+        if (TextUtils.equals(textInput.text, if (textInput.isFocused) defaultValue else "")) {
+            textInput.text = if (textInput.isFocused) "" else defaultValue
         }
     }
 
-    private void onPlay() {
-        CharSequence sourceUrl = viewBinding.sourceUrlTextInput.getText();
-        String defaultSourceUrl = (String) viewBinding.sourceUrlTextInput.getTag();
-
-        CharSequence adUrl = viewBinding.adUrlTextInput.getText();
-        String defaultAdUrl = (String) viewBinding.adUrlTextInput.getTag();
-
-        String adTimeOffset = (String) viewBinding.adPlacementsGroup.getTag(viewBinding.adPlacementsGroup.getCheckedRadioButtonId());
-        boolean isVmapChecked = viewBinding.adStandardVmap.isChecked();
-
-        PlayerActivity.play(
-                this,
-                TextUtils.isEmpty(sourceUrl) ? defaultSourceUrl : sourceUrl.toString(),
-                TextUtils.isEmpty(adUrl) ? defaultAdUrl : adUrl.toString(),
-                isVmapChecked ? null : adTimeOffset
-        );
+    private fun onPlay() {
+        val sourceUrl: CharSequence? = viewBinding.sourceUrlTextInput.text
+        val defaultSourceUrl = viewBinding.sourceUrlTextInput.tag as String
+        val adUrl: CharSequence? = viewBinding.adUrlTextInput.text
+        val defaultAdUrl = viewBinding.adUrlTextInput.tag as String
+        val adTimeOffset =
+            viewBinding.adPlacementsGroup.getTag(viewBinding.adPlacementsGroup.checkedRadioButtonId) as String
+        val isVmapChecked = viewBinding.adStandardVmap.isChecked
+        play(
+            this,
+            if (TextUtils.isEmpty(sourceUrl)) defaultSourceUrl else sourceUrl.toString(),
+            if (TextUtils.isEmpty(adUrl)) defaultAdUrl else adUrl.toString(),
+            if (isVmapChecked) "" else adTimeOffset
+        )
     }
-
 }
