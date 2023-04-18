36439e26ed64d6e0e3369ccbfaa0298ccf59eeb6
OlegRyz <aleh.ryzhankou@gmail.com>
2023-04-18 14:39:10 +0200

    Update Offline-Playback project

--- Offline-Playback/app/src/main/java/com/theoplayer/sample/playback/offline/SourceDescriptionRepository.java
+++ Offline-Playback/app/src/main/java/com/theoplayer/sample/playback/offline/SourceDescriptionRepository.java
@@ -27,43 +27,41 @@ public final class SourceDescriptionRepository {
     }
 
     private static SourceDescription getBigBuckBunnySourceDescription(Context context) {
-        return SourceDescription.Builder.sourceDescription(
-                TypedSource.Builder
-                        .typedSource(context.getString(R.string.bigBuckBunnySourceUrl))
+        return new SourceDescription.Builder(
+                new TypedSource.Builder(context.getString(R.string.bigBuckBunnySourceUrl))
                         .drm(
-                                DRMConfiguration.Builder.widevineDrm(
+                                new DRMConfiguration.Builder().widevine(
                                         // Note that license has to have PERSISTENT type configured
                                         // to be cached and to allow offline playback.
-                                        KeySystemConfiguration.Builder
-                                                .keySystemConfiguration(context.getString(R.string.bigBuckBunnyLicenseUrl))
+                                        new KeySystemConfiguration
+                                                .Builder(context.getString(R.string.bigBuckBunnyLicenseUrl))
                                                 .licenseType(LicenseType.PERSISTENT)
                                                 .build()
                                 ).build()
                         )
-                        .setExperimentalRenderingEnabled(true)
                         .build()
         ).build();
     }
 
     private static SourceDescription getSintelSourceDescription(Context context) {
-        return SourceDescription.Builder.sourceDescription(
-                TypedSource.Builder.typedSource(
+        return new SourceDescription.Builder(
+                new TypedSource.Builder(
                         context.getString(R.string.sintelSourceUrl)
                 ).build()
         ).build();
     }
 
     private static SourceDescription getTearsOfSteelSourceDescription(Context context) {
-        return SourceDescription.Builder.sourceDescription(
-                TypedSource.Builder.typedSource(
+        return new SourceDescription.Builder(
+                new TypedSource.Builder(
                         context.getString(R.string.tearsOfStealSourceUrl)
                 ).build()
         ).build();
     }
 
     private static SourceDescription getElephantsDreamSourceDescription(Context context) {
-        return SourceDescription.Builder.sourceDescription(
-                TypedSource.Builder.typedSource(
+        return new SourceDescription.Builder(
+                new TypedSource.Builder(
                         context.getString(R.string.elephantsDreamSourceUrl)
                 ).build()
         ).build();
