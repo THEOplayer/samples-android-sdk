44358caf7ffdb91f769f97f5e00838f976ea7f29
George Choustoulakis <george@choustoulakis.com>
2024-06-11 21:55:28 +0200

    Use source from SourceManager

--- custom-surface-rendering/src/main/java/com/theoplayer/sample/surface/PlayerActivity.kt
+++ custom-surface-rendering/src/main/java/com/theoplayer/sample/surface/PlayerActivity.kt
@@ -12,8 +12,7 @@ import com.theoplayer.android.api.event.player.PlayerEventTypes
 import com.theoplayer.android.api.player.AspectRatio
 import com.theoplayer.android.api.player.Player
 import com.theoplayer.android.api.player.RenderingTarget
-import com.theoplayer.android.api.source.SourceDescription
-import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.sample.common.SourceManager
 import com.theoplayer.sample.surface.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
@@ -49,16 +48,8 @@ class PlayerActivity : AppCompatActivity() {
         // and will also exit fullscreen when the device is rotated back to portrait.
         viewBinding.theoPlayerView.fullScreenManager.isFullScreenOrientationCoupled = true
 
-        // Creating a TypedSource builder that defines the location of a single stream source.
-        val typedSource = TypedSource.Builder(getString(R.string.defaultSourceUrl))
-
-        // Creating a SourceDescription builder that contains the settings to be applied as a new
-        // THEOplayer source.
-        val sourceDescription = SourceDescription.Builder(typedSource.build())
-            .poster(getString(R.string.defaultPosterUrl))
-
         // Configuring THEOplayer with defined SourceDescription object.
-        theoPlayer.source = sourceDescription.build()
+        theoPlayer.source = SourceManager.ELEPHANTS_DREAM_HLS
 
         //  Set autoplay to start video whenever player is visible
         theoPlayer.isAutoplay = true
