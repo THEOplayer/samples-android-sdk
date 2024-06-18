0c60f4d87c6d334548203af1789dceeb06d7e025
George Choustoulakis <george.choustoulakis@theoplayer.com>
2024-06-18 15:06:59 +0200

    Fix google-ima sample app

--- common/src/main/java/com/theoplayer/sample/common/SourceManager.kt
+++ common/src/main/java/com/theoplayer/sample/common/SourceManager.kt
@@ -2,6 +2,7 @@ package com.theoplayer.sample.common
 
 import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.api.source.TypedSource
+import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
 
 
 class SourceManager private constructor() {
@@ -11,6 +12,34 @@ class SourceManager private constructor() {
                 TypedSource.Builder("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
                     .build()
             )
+                .poster("https://cdn.theoplayer.com/video/elephants-dream/playlist.png")
+                .build()
+
+        }
+        val HLS_WITH_VMAP: SourceDescription by lazy {
+            SourceDescription.Builder(
+                TypedSource.Builder("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
+                    .build()
+            )
+                .ads(
+                    GoogleImaAdDescription
+                        .Builder("https://cdn.theoplayer.com/demos/ads/vmap/single-pre-mid-post-no-skip.xml")
+                        .build()
+                )
+                .poster("https://cdn.theoplayer.com/video/elephants-dream/playlist.png")
+                .build()
+
+        }
+        val HLS_WITH_VAST_PRE_ROLL: SourceDescription by lazy {
+            SourceDescription.Builder(
+                TypedSource.Builder("https://cdn.theoplayer.com/video/elephants-dream/playlist.m3u8")
+                    .build()
+            )
+                .ads(
+                    GoogleImaAdDescription
+                        .Builder("https://cdn.theoplayer.com/demos/ads/vast/dfp-preroll-no-skip.xml")
+                        .build()
+                )
                 .poster("https://cdn.theoplayer.com/video/elephants-dream/playlist.png")
                 .build()
 
--- google-ima/src/main/java/com/theoplayer/sample/ads/googleima/PlayerActivity.kt
+++ google-ima/src/main/java/com/theoplayer/sample/ads/googleima/PlayerActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.theoplayer.android.api.ads.ima.GoogleImaIntegration
+import com.theoplayer.android.api.ads.ima.GoogleImaIntegrationFactory
 import com.theoplayer.android.api.event.ads.AdBeginEvent
 import com.theoplayer.android.api.event.ads.AdEndEvent
 import com.theoplayer.android.api.event.ads.AdErrorEvent
@@ -11,10 +13,8 @@ import com.theoplayer.android.api.event.ads.AdsEventTypes
 import com.theoplayer.android.api.event.player.ErrorEvent
 import com.theoplayer.android.api.event.player.PlayerEventTypes
 import com.theoplayer.android.api.player.Player
-import com.theoplayer.android.api.source.SourceDescription
-import com.theoplayer.android.api.source.TypedSource
-import com.theoplayer.android.api.source.addescription.GoogleImaAdDescription
 import com.theoplayer.sample.ads.googleima.databinding.ActivityPlayerBinding
+import com.theoplayer.sample.common.SourceManager
 
 class PlayerActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityPlayerBinding
@@ -23,52 +23,24 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(com.theoplayer.sample.common.R.style.TheoTheme_Base)
         super.onCreate(savedInstanceState)
 
-        // Inflating view and obtaining an instance of the binding class.
+        // See basic-playback's PlayerActivity for mor information about basic setup.
         viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player)
-
-        // Gathering THEO objects references.
         theoPlayer = viewBinding.theoPlayerView.player
-
-        // Configuring action bar.
         setSupportActionBar(viewBinding.toolbarLayout.toolbar)
+        viewBinding.theoPlayerView.fullScreenManager.isFullScreenOrientationCoupled = true
+        theoPlayer.isAutoplay = true
 
-        // Configuring THEOplayer playback with default parameters.
-        configureTHEOplayer()
+        // Create the Google-IMA integration & add it to the player.
+        val googleImaIntegration: GoogleImaIntegration = GoogleImaIntegrationFactory.createGoogleImaIntegration(viewBinding.theoPlayerView)
+        theoPlayer.addIntegration(googleImaIntegration)
+
+        // Set a source with ads on the player.
+        theoPlayer.source = SourceManager.HLS_WITH_VMAP
+
+        attachEventListeners()
     }
 
-    private fun configureTHEOplayer() {
-
-        // Coupling the orientation of the device with the fullscreen state.
-        // The player will go fullscreen when the device is rotated to landscape
-        // and will also exit fullscreen when the device is rotated back to portrait.
-        viewBinding.theoPlayerView.fullScreenManager.isFullScreenOrientationCoupled = true
-
-        // Creating a TypedSource builder that defines the location of a single stream source.
-        val typedSource = TypedSource.Builder(getString(R.string.defaultSourceUrl))
-
-        // Creating a SourceDescription builder that contains the settings to be applied as a new
-        // THEOplayer source.
-        val sourceDescription = SourceDescription.Builder(typedSource.build())
-            .poster(getString(R.string.defaultPosterUrl))
-
-        // VMAP standard defines ads playlist and contains ads time offset definitions. To avoid
-        // overlapping, VMAP ads are defined separately.
-        if (resources.getBoolean(R.bool.loadVmapAds)) {
-            sourceDescription.ads( // Inserting linear pre-roll, mid-roll (15s) and post-roll ads defined with VMAP standard.
-                GoogleImaAdDescription.Builder(getString(R.string.defaultVmapAdUrl))
-                    .build()
-            )
-        } else {
-            sourceDescription.ads( // Inserting linear pre-roll ad defined with VAST standard.
-                GoogleImaAdDescription.Builder(getString(R.string.defaultVastLinearPreRollAdUrl))
-                    .timeOffset("start")
-                    .build(),
-            )
-        }
-
-        // Configuring THEOplayer with defined SourceDescription object.
-        theoPlayer.source = sourceDescription.build()
-
+    private fun attachEventListeners() {
         // Adding listeners to THEOplayer basic playback events.
         theoPlayer.addEventListener(PlayerEventTypes.PLAY) {
             Log.i(TAG,"Event: PLAY")
@@ -98,10 +70,6 @@ class PlayerActivity : AppCompatActivity() {
         }
     }
 
-    // In order to work properly and in sync with the activity lifecycle changes (e.g. device
-    // is rotated, new activity is started or app is moved to background) we need to call
-    // the "onResume", "onPause" and "onDestroy" methods of the THEOplayerView when the matching
-    // activity methods are called.
     override fun onPause() {
         super.onPause()
         viewBinding.theoPlayerView.onPause()
