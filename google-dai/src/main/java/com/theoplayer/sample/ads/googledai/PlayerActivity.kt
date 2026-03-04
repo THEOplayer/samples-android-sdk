6db217331cd50c2832f876392405658ff06c3629
Ceyhun Ozgoc <ceyhun.ozgoc@theoplayer.com>
2026-03-04 16:13:59 +0100

    Update Google DAI sample

--- google-dai/src/main/java/com/theoplayer/sample/ads/googledai/PlayerActivity.kt
+++ google-dai/src/main/java/com/theoplayer/sample/ads/googledai/PlayerActivity.kt
@@ -2,138 +2,169 @@ package com.theoplayer.sample.ads.googledai
 
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.theoplayer.android.api.THEOplayerConfig
 import com.theoplayer.android.api.THEOplayerGlobal
+import com.theoplayer.android.api.THEOplayerView
 import com.theoplayer.android.api.event.ads.AdsEventTypes
 import com.theoplayer.android.api.event.player.ErrorEvent
 import com.theoplayer.android.api.event.player.PlayerEventTypes
-import com.theoplayer.android.api.player.Player
-import com.theoplayer.sample.ads.googledai.databinding.ActivityPlayerBinding
+import com.theoplayer.android.ui.DefaultUI
+import com.theoplayer.android.ui.rememberPlayer
+import com.theoplayer.android.ui.theme.THEOplayerTheme
+import com.theoplayer.sample.common.AppTopBar
 import com.theoplayer.sample.common.SourceManager
 
-class PlayerActivity : AppCompatActivity() {
-    private lateinit var viewBinding: ActivityPlayerBinding
-    private lateinit var theoPlayer: Player
+class PlayerActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(com.theoplayer.sample.common.R.style.TheoTheme_Base)
+        // Enable all debug logs from THEOplayer.
+        THEOplayerGlobal.getSharedInstance(this).logger.enableAllTags()
+
         super.onCreate(savedInstanceState)
 
-        // See basic-playback's PlayerActivity for more information about basic setup.
-        viewBinding = DataBindingUtil.setContentView(this, R.layout.activity_player)
-        theoPlayer = viewBinding.theoPlayerView.player
+        setContent {
+            val context = LocalContext.current
+            val theoplayerView = remember(context) {
+                THEOplayerView(context, THEOplayerConfig.Builder().build()).apply {
+                    keepScreenOn = true
+                }
+            }
+            val player = rememberPlayer(theoplayerView)
+            val theoPlayer = theoplayerView.player
 
-        // Enable all debug logs from THEOplayer.
-        val theoDebugLogger = THEOplayerGlobal.getSharedInstance(this).logger
-        theoDebugLogger.enableAllTags()
+            LaunchedEffect(player) {
+                // Coupling the orientation of the device with the fullscreen state.
+                theoplayerView.fullScreenManager.isFullScreenOrientationCoupled = true
 
-        setSupportActionBar(viewBinding.toolbarLayout.toolbar)
-        viewBinding.theoPlayerView.fullScreenManager.isFullScreenOrientationCoupled = true
+                // THEOplayer automatically adds all available integrations to the player.
+                // Alternatively, you can set autoIntegrations(false) on your player configuration
+                // and add them manually.
+//                val googleDaiIntegration = GoogleDaiIntegrationFactory.createGoogleDaiIntegration(theoplayerView)
+//                theoPlayer.addIntegration(googleDaiIntegration)
 
-        // Keep the device screen on.
-        viewBinding.theoPlayerView.keepScreenOn = true
+                // Set a source with ads on the player.
+                theoPlayer.source = SourceManager.DAI_HLS_LIVE
 
-        //  Set autoplay to start video whenever player is visible.
-        theoPlayer.isAutoplay = true
+                // Set autoplay to start video whenever player is visible.
+                theoPlayer.isAutoplay = true
 
-        // THEOplayer automatically adds all available integrations to the player. Alternatively, you can set autoIntegrations(false) on your player configuration and add them manually.
-//        val googleDaiIntegration: GoogleDaiIntegration = GoogleDaiIntegrationFactory.createGoogleDaiIntegration(viewBinding.theoPlayerView)
-//        theoPlayer.addIntegration(googleDaiIntegration)
+                // Attach player event listeners.
+                theoPlayer.addEventListener(PlayerEventTypes.SOURCECHANGE) {
+                    Log.i(TAG, "Event: SOURCECHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.CURRENTSOURCECHANGE) {
+                    Log.i(TAG, "Event: CURRENTSOURCECHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.LOADEDDATA) {
+                    Log.i(TAG, "Event: LOADEDDATA")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.LOADEDMETADATA) {
+                    Log.i(TAG, "Event: LOADEDMETADATA")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.DURATIONCHANGE) {
+                    Log.i(TAG, "Event: DURATIONCHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.TIMEUPDATE) {
+//                    Log.i(TAG, "Event: TIMEUPDATE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.PLAY) {
+                    Log.i(TAG, "Event: PLAY")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.PLAYING) {
+                    Log.i(TAG, "Event: PLAYING")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.PAUSE) {
+                    Log.i(TAG, "Event: PAUSE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.SEEKING) {
+                    Log.i(TAG, "Event: SEEKING")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.SEEKED) {
+                    Log.i(TAG, "Event: SEEKED")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.WAITING) {
+                    Log.i(TAG, "Event: WAITING")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.READYSTATECHANGE) {
+                    Log.i(TAG, "Event: READYSTATECHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.PRESENTATIONMODECHANGE) {
+                    Log.i(TAG, "Event: PRESENTATIONMODECHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.VOLUMECHANGE) {
+                    Log.i(TAG, "Event: VOLUMECHANGE")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.ENDED) {
+                    Log.i(TAG, "Event: ENDED")
+                }
+                theoPlayer.addEventListener(PlayerEventTypes.ERROR) { event: ErrorEvent ->
+                    Log.i(TAG, "Event: ERROR, error=" + event.errorObject)
+                }
 
-        // Set a source with ads on the player.
-        theoPlayer.source = SourceManager.DAI_HLS_LIVE
+                // Attach ad event listeners.
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_BEGIN) {
+                    Log.i(TAG, "Event: AD_BEGIN")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_END) {
+                    Log.i(TAG, "Event: AD_END")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_ERROR) {
+                    Log.i(TAG, "Event: AD_ERROR")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_LOADED) {
+                    Log.i(TAG, "Event: AD_LOADED")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_BREAK_BEGIN) {
+                    Log.i(TAG, "Event: AD_BREAK_BEGIN")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_BREAK_END) {
+                    Log.i(TAG, "Event: AD_BREAK_END")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_SKIP) {
+                    Log.i(TAG, "Event: AD_SKIP")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_TAPPED) {
+                    Log.i(TAG, "Event: AD_TAPPED")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_IMPRESSION) {
+                    Log.i(TAG, "Event: AD_IMPRESSION")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_FIRST_QUARTILE) {
+                    Log.i(TAG, "Event: AD_FIRST_QUARTILE")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_MIDPOINT) {
+                    Log.i(TAG, "Event: AD_MIDPOINT")
+                }
+                theoPlayer.ads.addEventListener(AdsEventTypes.AD_THIRD_QUARTILE) {
+                    Log.i(TAG, "Event: AD_THIRD_QUARTILE")
+                }
+            }
 
-        attachEventListeners()
-    }
-
-    private fun attachEventListeners() {
-        // Adding listeners to THEOplayer basic playback events.
-        theoPlayer.addEventListener(PlayerEventTypes.PLAY) {
-            Log.i(TAG, "Event: PLAY")
+            THEOplayerTheme(useDarkTheme = true) {
+                Scaffold(
+                    topBar = { AppTopBar() }
+                ) { padding ->
+                    DefaultUI(
+                        modifier = Modifier
+                            .padding(padding)
+                            .fillMaxSize(),
+                        player = player
+                    )
+                }
+            }
         }
-        theoPlayer.addEventListener(PlayerEventTypes.PLAYING) {
-            Log.i(TAG, "Event: PLAYING")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.PAUSE) {
-            Log.i(TAG, "Event: PAUSE")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.SEEKING) {
-            Log.i(TAG, "Event: SEEKING")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.SEEKED) {
-            Log.i(TAG, "Event: SEEKED")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.LOADEDDATA) {
-            Log.i(TAG, "Event: LOADEDDATA")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.LOADEDMETADATA) {
-            Log.i(TAG, "Event: LOADEDMETADATA")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.WAITING) {
-            Log.i(TAG, "Event: WAITING")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.ENDED) {
-            Log.i(TAG, "Event: ENDED")
-        }
-        theoPlayer.addEventListener(PlayerEventTypes.ERROR) { event: ErrorEvent ->
-            Log.i(TAG, "Event: ERROR, error=" + event.errorObject)
-        }
-
-        // Adding listeners to THEOplayer basic ad events.
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_BEGIN) {
-            Log.i(TAG, "Event: AD_BEGIN")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_END) {
-            Log.i(TAG,"Event: AD_END")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_ERROR) {
-            Log.i(TAG, "Event: AD_ERROR")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_LOADED) {
-            Log.i(TAG, "Event: AD_LOADED")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_BREAK_BEGIN) {
-            Log.i(TAG, "Event: AD_BREAK_BEGIN")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_BREAK_END) {
-            Log.i(TAG, "Event: AD_BREAK_END")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_SKIP) {
-            Log.i(TAG, "Event: AD_SKIP")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_TAPPED) {
-            Log.i(TAG, "Event: AD_TAPPED")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_IMPRESSION) {
-            Log.i(TAG, "Event: AD_IMPRESSION")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_FIRST_QUARTILE) {
-            Log.i(TAG, "Event: AD_FIRST_QUARTILE")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_MIDPOINT) {
-            Log.i(TAG, "Event: AD_MIDPOINT")
-        }
-        theoPlayer.ads.addEventListener(AdsEventTypes.AD_THIRD_QUARTILE) {
-            Log.i(TAG, "Event: AD_THIRD_QUARTILE")
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        viewBinding.theoPlayerView.onPause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        viewBinding.theoPlayerView.onResume()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        viewBinding.theoPlayerView.onDestroy()
     }
 
     companion object {
-        private val TAG = PlayerActivity::class.java.simpleName
+        private val TAG: String = PlayerActivity::class.java.simpleName
     }
 }
