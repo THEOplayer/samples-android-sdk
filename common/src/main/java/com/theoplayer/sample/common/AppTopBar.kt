1516a0ad86e3cba8fdcf2e8f693e417a6170c60f
Mattias Buelens <mattias.buelens@theoplayer.com>
2026-03-19 15:34:07 +0100

    Add navigateBack to AppTopBar

--- common/src/main/java/com/theoplayer/sample/common/AppTopBar.kt
+++ common/src/main/java/com/theoplayer/sample/common/AppTopBar.kt
@@ -2,12 +2,15 @@ package com.theoplayer.sample.common
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.theoplayer.android.api.THEOplayerGlobal
 
@@ -15,13 +18,24 @@ import com.theoplayer.android.api.THEOplayerGlobal
 @Composable
 fun AppTopBar(
     title: String = stringResource(R.string.appDisplayName),
-    actions: @Composable RowScope.() -> Unit = {}
+    actions: @Composable RowScope.() -> Unit = {},
+    navigateBack: (() -> Unit)? = null
 ) {
     TopAppBar(
         title = { Text(title) },
         actions = {
             actions()
             Text(text = "v" + THEOplayerGlobal.getVersion())
+        },
+        navigationIcon = {
+            navigateBack?.let {
+                IconButton(onClick = navigateBack) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_arrow_back),
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = colorResource(R.color.dolbyPurple),
